8d8925edd3b5fcc4db2cc82e4c46e2598acd632d
EvaristoGZ <evgarciaz@gmail.com>
2014-02-12 14:08:30 +0100

    Obtiene los datos de la API a través de requests y muestra datos.text

--- tiempo.py
+++ tiempo.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import requests
 import json
 print "Aplicación base || OpenWeatherMap"
 print ""
@@ -10,4 +11,8 @@ for elemento in prov:
 valor = (int(raw_input("\n¿De qué ciudad quieres saber la temperatura actual?\n"))-1)
 provlimp = (prov[valor][3:])
 
-print "\nLa temperatura actual de %s es la siguiente:\n" % (provlimp)
+print "\nLa temperatura actual de %s es la siguiente:\n" % provlimp
+
+datos = requests.get('http://api.openweathermap.org/data/2.5/weather',params={'q':'%s,spain' % provlimp})
+
+print datos.text
